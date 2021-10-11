6b1f5ef33fc0e64f8bfcf9c2fb392a024f17e993
Atilin <71184528+Atilin@users.noreply.github.com>
2021-10-11 18:18:17 +0300

    Обратная польская запись

---
+++ 2021.10.07/Обратная польская запись/Обратная польская запись/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+void f1(string s, string r)
+{
+	for (int i = 0; i < s.size(); ++i)
+	{
+		if 
+	}
+}
+
+int main()
+{
+	string s;
+	cin >> s;
+	string r;
+
+	return EXIT_SUCCESS;
+}
